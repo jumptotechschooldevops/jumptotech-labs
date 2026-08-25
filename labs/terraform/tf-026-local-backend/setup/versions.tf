af47f85f6a382527058fc35d551ca9ea02aa0d99
Aisalkyn Aidarova <aisalkynaidarova8@gmail.com>
2026-08-25 18:20:57 -0500

    feat(terraform): add TF-026 local backend lab

---
+++ labs/terraform/tf-026-local-backend/setup/versions.tf
@@ -0,0 +1,8 @@
+terraform {
+  required_version = ">= 1.5.0"
+
+  required_providers {
+    local  = { source = "hashicorp/local", version = "2.5.2" }
+    random = { source = "hashicorp/random", version = "3.6.3" }
+  }
+}
